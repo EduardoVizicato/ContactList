f0468ca6d1fa67584328816c3d3671fcc56a9992
EduardoVizicato <eduardovizicato@gmail.com>
2024-05-10 19:42:20 -0300

    NavBar works

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CadastroComponent } from '../cadastro/cadastro.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CadastroComponent],
+  imports: [CadastroComponent, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
